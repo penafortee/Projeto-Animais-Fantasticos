47308014d852e9bb0e66df3ba6235892b8b06068
penafortee <nathipenaforte@gmail.com>
2024-01-02 19:01:05 -0300

    -

--- JavaScript/modules/menu-mobile.js
+++ JavaScript/modules/menu-mobile.js
@@ -10,6 +10,7 @@ export default function initMenuMobile() {
       menuList.classList.add("active");
       menuButton.classList.add("active");
       outsideClick(menuList, eventos, () => {
+        console.log("teste");
         menuList.classList.remove("active");
         menuButton.classList.remove("active");
       });
